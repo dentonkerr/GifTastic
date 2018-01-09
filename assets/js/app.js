608b10a1994cc51121b341cfb2d0869dd418e769
Denton Kerr <dentonkerr@Dentons-MacBook-Pro.local>
2018-01-09 11:58:21 -0500

    adding appended gifs on click event

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,12 +22,41 @@ $("#add-sport").on("click", function(event) {
 
     var topic = $("#sport-input").val().trim();
 
+    //adds button, need data-name & class designation
     $("#topic-buttons").append("<button>" + topic + "</button>");
 
 });
 
 createButtons();
 
+$("button").on("click", function() {
+
+    var clickedBtn = $(this).attr("data-name");
+    console.log(clickedBtn);
+
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + clickedBtn + "&api_key=OQsUJXjxQqapDlxqn92Zbiv5jzfHI9z2&limit=10";
+    console.log(queryURL);
+
+    $.ajax({
+        
+        url: queryURL,
+        
+        method: "GET"
+      
+    })
+      
+      .done(function(response) {
+      
+        var gifs = response.data;
+      
+      for (var i = 0; i < gifs.length; i++) {
+        
+        var image = $("<img>").attr("src", gifs[i].images.downsized.url);
+        console.log(image);
+        
+        $("#gifs-here").append(image);
+    }
+})});
 
 
 
